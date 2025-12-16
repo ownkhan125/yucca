f24296924a6b6ad95872c64a8ee41d305dda24c3
Aun Khan <107195320+ownkhan125@users.noreply.github.com>
2025-12-16 12:25:36 +0500

    refactor: restructure layout and styling components; remove unused globals.css and page.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Orbitron } from "next/font/google";
+import "../style/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const orbitron = Orbitron({
+  variable: "--font-orbitron",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata = {
@@ -19,11 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${orbitron.variable} antialiased`}>{children}</body>
     </html>
   );
 }
